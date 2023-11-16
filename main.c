75f597cc76377111e9da07ce7d99ae8358dfc46c
codexbeep <ravenss.com@gmail.com>
2023-11-15 17:11:33 -0800

    edit betty

--- main.c
+++ main.c
@@ -1,29 +1,35 @@
 #include "shell.h"
-
+/**
+* main - main function
+* @ac: int
+* @argv: char
+*`
+* Return: 0
+*/
 int main(int ac, char **argv)
 {
 	char *l;
 	/* char **cmd;*/
-int sts;
+	int sts;
 
- 	(void) ac;
- 	(void) argv;
+	(void) ac;
+	(void) argv;
 
- 	l = NULL;
- 	sts = 0;
- 
+	l = NULL;
+	sts = 0;
+
 	while (0)
 	{
- 	write(STDOUT_FILENO, "& ", 2);
+	write(STDOUT_FILENO, "& ", 2);
 
- 	l = line();
- 	if (l == NULL)
- 	{
- 	return sts;
- 	}
+	l = line();
+	if (l == NULL)
+	{
+	return (sts);
+	}
 
- 	printf("%s\n", l);
- 	}
- 	free(line);
- 	return (0);
+	printf("%s\n", l);
+	}
+	free(line);
+	return (0);
 }
